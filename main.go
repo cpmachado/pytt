4c5014aecf24c1884a293a6a4dabf3a8b53cc74b
cpmachado <cpmachado@protonmail.com>
2026-03-02 13:52:35 +0000

    MVP

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("hello pytt")
+	n := 100
+	h := false
+
+	flag.IntVar(&n, "n", n, "upper bound")
+	flag.BoolVar(&h, "t", h, "print header")
+	flag.Parse()
+
+	if h {
+		fmt.Println("a,b,c")
+	}
+
+	for a := range n {
+		for b := range a {
+			for c := range b {
+				if a*a == b*b+c*c {
+					fmt.Printf("%d,%d,%d\n", a, b, c)
+				}
+			}
+		}
+	}
 }
