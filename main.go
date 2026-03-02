46efba74dd1f8d31440d34ec618213379264e190
cpmachado <cpmachado@protonmail.com>
2026-03-02 13:58:11 +0000

    Add custom usage

--- main.go
+++ main.go
@@ -5,12 +5,24 @@ import (
 	"fmt"
 )
 
+const usageContent = `%s is a simple tool to compute pythagorean triplets, in csv form.
+
+Usage of %s:
+  -n int
+    	upper bound (default 100)
+  -t	print header
+`
+
 func main() {
 	n := 100
 	h := false
 
 	flag.IntVar(&n, "n", n, "upper bound")
 	flag.BoolVar(&h, "t", h, "print header")
+	flag.Usage = func() {
+		fmt.Printf(usageContent, flag.CommandLine.Name(), flag.CommandLine.Name())
+	}
+
 	flag.Parse()
 
 	if h {
