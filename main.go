18bd56bbe2732d703c347541e89e01802dd0144c
cpmachado <cpmachado@protonmail.com>
2026-03-10 22:27:23 +0000

    Add version flag

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"runtime/debug"
 )
 
 const usageContent = `%s is a simple tool to compute pythagorean triplets, in csv form.
@@ -11,19 +13,31 @@ Usage of %s:
   -n int
     	upper bound of each element (default 100)
   -t	print header
+  -v	print version
 `
 
 func main() {
 	n := 100
 	h := false
+	v := false
 
 	flag.IntVar(&n, "n", n, "upper bound of each element")
 	flag.BoolVar(&h, "t", h, "print header")
+	flag.BoolVar(&v, "v", v, "display version")
 	flag.Usage = func() {
 		fmt.Printf(usageContent, flag.CommandLine.Name(), flag.CommandLine.Name())
 	}
 
 	flag.Parse()
+
+	if v {
+		vsn := "dev"
+		if info, ok := debug.ReadBuildInfo(); ok {
+			vsn = info.Main.Version
+		}
+		fmt.Printf("pytt-%s\n", vsn)
+		os.Exit(0)
+	}
 
 	if h {
 		fmt.Println("a,b,c")
